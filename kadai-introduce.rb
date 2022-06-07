42aae338a63ca6902239275ff0ca1259401fc318
kazuki.natsume <natsume.business@gmail.com>
2022-06-07 12:57:14 +0000

    modified kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,8 +1,6 @@
 name_myouji = '棗'
 name_namae = '和貴'
-str1 ='です。'
 
 age = 30
-str2 ='歳です。'
 
-puts name_myouji + name_namae + str1 + age.to_s + str2 
+puts name_myouji + name_namae + 'です。' + age.to_s + '歳です。' 
